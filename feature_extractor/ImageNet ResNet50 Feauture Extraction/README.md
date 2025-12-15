162c16fa3948c94caf22a4702ee37c90d35426f2
Manuel Morteo <mmorteo@scc-j08.scc.bu.edu>
2025-12-15 00:43:28 -0500

    adding feature extraction files

---
+++ feature_extractor/ImageNet ResNet50 Feauture Extraction/README.md
@@ -0,0 +1,1 @@
+graphs_all in this directory contains all the graphs built using ImageNet Frozen Weights
--- feature_extractor/run.py
+++ feature_extractor/run.py
@@ -4,17 +4,24 @@ from data_aug.dataset_wrapper import DataSetWrapper
 import os, glob
 import pandas as pd
 import argparse
+import sys
+import gc
 
 def main():
+    sys.argv = [sys.argv[0]]
+    
     parser = argparse.ArgumentParser()
     parser.add_argument('--magnification', type=str, default='20x')
     args = parser.parse_args()
     config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
-    dataset = DataSetWrapper(config['batch_size'], **config['dataset'])
     
+    # Use single GPU - simpler and works reliably
+    config['n_gpu'] = 1
+    config['gpu_ids'] = "[0]"
+   
+    dataset = DataSetWrapper(config['batch_size'], **config['dataset'])
     simclr = SimCLR(dataset, config)
     simclr.train()
-
-
+    
 if __name__ == "__main__":
     main()
